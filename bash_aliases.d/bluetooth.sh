fed21922e47abb00623b23f1fa0fdc80797b5f4a
Greg Mitchell <greg.mitchell@tuxsoft.net>
2022-06-01 11:57:36 -0400

    remove vim flatkpak, + misc

--- bash_aliases.d/bluetooth.sh
+++ bash_aliases.d/bluetooth.sh
@@ -1,3 +1,4 @@
-alias btsony='bluetoothctl connect 04:5D:4B:83:A8:57'
-alias bttao='bluetoothctl connect E8:AB:FA:30:E3:0C'
+alias btsony='rfkill unblock bluetooth; sleep 5; bluetoothctl connect 04:5D:4B:83:A8:57'
+alias bttao='rfkill unblock bluetooth; sleep 5;  bluetoothctl connect E8:AB:FA:30:E3:0C'
+alias btfunsound='rfkill unblock bluetooth; sleep 5; bluetoothctl connect 41:42:6F:40:A2:B8'
 alias btdis='bluetoothctl disconnect'
