5134db59fe7693598dd98f22f348ef3c83ebc5e2
Greg Mitchell <greg.mitchell@tuxsoft.net>
2024-01-01 00:13:36 -0500

    bt changes; ssh config

--- bash_aliases.d/bluetooth.sh
+++ bash_aliases.d/bluetooth.sh
@@ -1,4 +1,4 @@
 alias btsony='rfkill unblock bluetooth; sleep 5; bluetoothctl connect 04:5D:4B:83:A8:57'
 alias bttao='rfkill unblock bluetooth; sleep 5;  bluetoothctl connect E8:AB:FA:30:E3:0C'
-alias btfunsound='rfkill unblock bluetooth; sleep 5; bluetoothctl connect 41:42:6F:40:A2:B8'
+alias btfunsound='rfkill unblock bluetooth; sleep 5; bluetoothctl connect 41:42:36:E1:25:ED'
 alias btdis='bluetoothctl disconnect'
